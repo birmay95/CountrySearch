6aece0689897484c1e39851dd7241da16fc74b9b
Mishail Borovensky <borovenskyy@gmail.com>
2024-03-18 20:01:37 +0300

    Commit for SonarCloud

---
+++ src/main/java/com/mishail/country_search/cache/CacheService.java
@@ -0,0 +1,33 @@
+package com.mishail.country_search.cache;
+
+import org.springframework.stereotype.Service;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Service
+public class CacheService {
+
+    private final Map<String, Object> cache = new ConcurrentHashMap<>();
+
+    public void put(String key, Object value) {
+        cache.put(key, value);
+    }
+
+    public Object get(String key) {
+        return cache.get(key);
+    }
+
+    public boolean containsKey(String key) {
+        return cache.containsKey(key);
+    }
+
+    public void evict(String key) {
+        cache.remove(key);
+    }
+
+    public void clear() {
+        cache.clear();
+    }
+}
+
